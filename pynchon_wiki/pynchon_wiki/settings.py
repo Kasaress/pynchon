e2be3bd0398bbf11ce84a7923897dcf1121114e7
Egor Remezov <mr.drode@gmail.com>
2023-03-10 11:49:49 +0300

    fix static settings

--- pynchon_wiki/pynchon_wiki/settings.py
+++ pynchon_wiki/pynchon_wiki/settings.py
@@ -110,7 +110,7 @@ STATIC_URL = '/static/'
 if DEBUG:
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'wiki/static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
