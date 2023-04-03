ad54f484d732c02bea668685f5be2afc2bb5dfc9
Egor Remezov <mr.drodel@gmail.com>
2023-04-03 17:21:22 +0300

    поправил порядок полей в админке у меню

--- pynchon_wiki/core/admin.py
+++ pynchon_wiki/core/admin.py
@@ -8,3 +8,15 @@ class TopMenuAdmin(admin.ModelAdmin):
     list_display = ('id', 'is_active', 'name', 'url', 'sort',)
     search_fields = ('name', 'url',)
     readonly_fields = ('created_at',)
+
+    fieldsets = (
+        ('Общее', {
+            'classes': ('wide',),
+            'fields': ('is_active', 'created_at', 'deleted_at',)
+        }),
+        ('Данные', {
+            'classes': ('wide',),
+            'fields': (
+                'name', 'url', 'sort', 'auth_only',)
+        }),
+    )
