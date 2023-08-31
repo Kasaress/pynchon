b12159d79f2f5c38f578fedb99f298084c0f29bc
Владислав Нестеров <nestern8@gmail.com>
2023-09-01 04:07:51 +0700

    fix view r5

--- pynchon_wiki/wiki/views.py
+++ pynchon_wiki/wiki/views.py
@@ -121,7 +121,7 @@ def rainbow_part5(request):
     context = {
         'book': book,
         'chapters': Chapter.objects.filter(book=book).all(),
-        'start_event': TableChronology.objects.get(id=1),
+        'start_event': TableChronology.objects.get(id=172),
         'events': TableChronology.objects.all(),
         'search_model': 'chronology'
     }
