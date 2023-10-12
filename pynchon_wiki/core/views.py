a836903dbebcc01bf10774bf18f02edef36205c7
Владислав Нестеров <nestern8@gmail.com>
2023-10-13 00:39:13 +0700

    fix 404

--- pynchon_wiki/core/views.py
+++ pynchon_wiki/core/views.py
@@ -5,16 +5,16 @@ from django.shortcuts import render
 # from services.time import get_datetime_today
 
 
-def page_not_found(request):
-    return render(request, 'core/404.html', {'path': request.path}, status=404)
+def page_not_found(request, exception):
+    return render(request, 'core/404.html', status=404)
 
 
 def csrf_failure(request):
     return render(request, 'core/403csrf.html')
 
 
-# def server_error(request):
-#     return render(request, 'core/500.html', status=500)
+def server_error(request):
+    return render(request, 'core/500.html', status=500)
 
 
 def permission_denied(request, exception):
