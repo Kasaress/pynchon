63bdb83569eb1046b8c44684c392a4fedb7f0e09
Владислав Нестеров <nestern8@gmail.com>
2024-03-18 22:50:10 +0700

    fix v index

--- pynchon_wiki/static/js/v-main.js
+++ pynchon_wiki/static/js/v-main.js
@@ -7,11 +7,27 @@ let dot = document.querySelector('.promo__v-dot');
 window.onScroll = magicV;
 
 function magicV() {
-  leftPart.style.transform = `rotate(${- 170 + 170 / (window.pageYOffset/200 + 1)}deg)`;
-  rightPart.style.transform = `rotate(${170 - 170 / (window.pageYOffset/200 + 1)}deg)`;
-  promoText.style.opacity = `${1/(window.pageYOffset + 1)}`;
-  if (window.pageYOffset > 2) {
-    dot.style.animation = 'pointBounce 2s ease-out forwards';
+  const scrollTop = window.pageYOffset;
+
+  leftPart.style.transform = `rotate(${- 170 + 170 / (scrollTop/200 + 1)}deg)`;
+  rightPart.style.transform = `rotate(${170 - 170 / (scrollTop/200 + 1)}deg)`;
+  promoText.style.opacity = `${1/(scrollTop + 1)}`;
+
+  const startOffset = 0;
+  const endOffset = 800;
+
+  const progress = (scrollTop - startOffset) / (endOffset - startOffset);
+
+  if (progress <= 0.1) {
+    dot.style.transform = `translateY(${- 70 * 10 * progress}px)`;
+  } else if (progress <= 0.2) {
+    dot.style.transform = `translateY(${-70 + (70 * 10 * (progress - 0.1))}px)`;
+  } else if (progress <= 0.3) {
+    dot.style.transform = `translateY(${- 20 * 10 * (progress - 0.2)}px)`;
+  } else if (progress <= 0.4) {
+    dot.style.transform = `translateY(${-20 + (20 * 10 * (progress - 0.3))}px)`;
+  } else {
+    dot.style.transform = `translateX(${50 * 10 * (progress - 0.4)}vw)`;
   }
 }
 
