06cbfe173b7094aa66a6bb152e64c7114af5a692
Владислав Нестеров <nestern8@gmail.com>
2024-06-11 13:16:45 +0700

    test fix

--- pynchon_wiki/wiki/views.py
+++ pynchon_wiki/wiki/views.py
@@ -1,5 +1,6 @@
-from itertools import chain
 import datetime as dt
+
+from itertools import chain
 from django.core.mail.message import EmailMessage
 from django.http import BadHeaderError, HttpResponse
 from django.shortcuts import get_object_or_404, render
